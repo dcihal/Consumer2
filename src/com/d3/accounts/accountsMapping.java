064518ebea3b1b652064da6caf735edd889e6901
dcihal <dcihal@gmail.com>
2015-01-06 11:01:23 -0600

    Adding test parameters

--- src/com/d3/accounts/accountsMapping.java
+++ src/com/d3/accounts/accountsMapping.java
@@ -1,5 +1,13 @@
 package com.d3.accounts;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class accountsMapping {
+	
+    @FindBy(how = How.CSS, using = "div.nav-icon.accounts.center")
+    
+    public WebElement Accounts;
 
 }
